cd0d982997a911775b5cd49cd4a46ab3d3b5ad34
mustafamertcakar <mustafacakarr1@gmail.com>
2024-09-28 13:37:49 +0300

    Hesap Makinesi
    C# Windows Form ile geliştirilmiştir.

---
+++ Hesap_Makinesi/Hesap Makinesi/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Tb = new System.Windows.Forms.TextBox();
+            this.SilBtn = new System.Windows.Forms.Button();
+            this.YuzdeBtn = new System.Windows.Forms.Button();
+            this.EksiArtiBtn = new System.Windows.Forms.Button();
+            this.BolBtn = new System.Windows.Forms.Button();
+            this.CarpBtn = new System.Windows.Forms.Button();
+            this.ArtiBtn = new System.Windows.Forms.Button();
+            this.EksiBtn = new System.Windows.Forms.Button();
+            this.EsitBtn = new System.Windows.Forms.Button();
+            this.NoktaBtn = new System.Windows.Forms.Button();
+            this.SifirBtn = new System.Windows.Forms.Button();
+            this.UcBtn = new System.Windows.Forms.Button();
+            this.İki = new System.Windows.Forms.Button();
+            this.DortBtn = new System.Windows.Forms.Button();
+            this.BesBtn = new System.Windows.Forms.Button();
+            this.AltiBtn = new System.Windows.Forms.Button();
+            this.DokuzBtn = new System.Windows.Forms.Button();
+            this.SekizBtn = new System.Windows.Forms.Button();
+            this.YediBtn = new System.Windows.Forms.Button();
+            this.BirBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Tb
+            // 
+            this.Tb.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tb.Location = new System.Drawing.Point(10, 13);
+            this.Tb.Margin = new System.Windows.Forms.Padding(4);
+            this.Tb.Name = "Tb";
+            this.Tb.ReadOnly = true;
+            this.Tb.Size = new System.Drawing.Size(420, 43);
+            this.Tb.TabIndex = 0;
+            this.Tb.Text = "0";
+            this.Tb.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // SilBtn
+            // 
+            this.SilBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SilBtn.Location = new System.Drawing.Point(281, 64);
+            this.SilBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.SilBtn.Name = "SilBtn";
+            this.SilBtn.Size = new System.Drawing.Size(148, 75);
+            this.SilBtn.TabIndex = 17;
+            this.SilBtn.Text = "Sil";
+            this.SilBtn.UseVisualStyleBackColor = true;
+            this.SilBtn.Click += new System.EventHandler(this.SilBtn_Click);
+            // 
+            // YuzdeBtn
+            // 
+            this.YuzdeBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.YuzdeBtn.Location = new System.Drawing.Point(360, 313);
+            this.YuzdeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.YuzdeBtn.Name = "YuzdeBtn";
+            this.YuzdeBtn.Size = new System.Drawing.Size(69, 75);
+            this.YuzdeBtn.TabIndex = 16;
+            this.YuzdeBtn.Text = "%";
+            this.YuzdeBtn.UseVisualStyleBackColor = true;
+            this.YuzdeBtn.Click += new System.EventHandler(this.YuzdeBtn_Click);
+            // 
+            // EksiArtiBtn
+            // 
+            this.EksiArtiBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.EksiArtiBtn.Location = new System.Drawing.Point(281, 313);
+            this.EksiArtiBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.EksiArtiBtn.Name = "EksiArtiBtn";
+            this.EksiArtiBtn.Size = new System.Drawing.Size(71, 75);
+            this.EksiArtiBtn.TabIndex = 20;
+            this.EksiArtiBtn.Text = "-/+";
+            this.EksiArtiBtn.UseVisualStyleBackColor = true;
+            this.EksiArtiBtn.Click += new System.EventHandler(this.EksiArtiBtn_Click);
+            // 
+            // BolBtn
+            // 
+            this.BolBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BolBtn.Location = new System.Drawing.Point(281, 230);
+            this.BolBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.BolBtn.Name = "BolBtn";
+            this.BolBtn.Size = new System.Drawing.Size(71, 75);
+            this.BolBtn.TabIndex = 19;
+            this.BolBtn.Text = "/";
+            this.BolBtn.UseVisualStyleBackColor = true;
+            this.BolBtn.Click += new System.EventHandler(this.BolBtn_Click);
+            // 
+            // CarpBtn
+            // 
+            this.CarpBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CarpBtn.Location = new System.Drawing.Point(360, 230);
+            this.CarpBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.CarpBtn.Name = "CarpBtn";
+            this.CarpBtn.Size = new System.Drawing.Size(69, 75);
+            this.CarpBtn.TabIndex = 15;
+            this.CarpBtn.Text = "x";
+            this.CarpBtn.UseVisualStyleBackColor = true;
+            this.CarpBtn.Click += new System.EventHandler(this.CarpBtn_Click);
+            // 
+            // ArtiBtn
+            // 
+            this.ArtiBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ArtiBtn.Location = new System.Drawing.Point(358, 147);
+            this.ArtiBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.ArtiBtn.Name = "ArtiBtn";
+            this.ArtiBtn.Size = new System.Drawing.Size(71, 75);
+            this.ArtiBtn.TabIndex = 14;
+            this.ArtiBtn.Text = "+";
+            this.ArtiBtn.UseVisualStyleBackColor = true;
+            this.ArtiBtn.Click += new System.EventHandler(this.ArtiBtn_Click);
+            // 
+            // EksiBtn
+            // 
+            this.EksiBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.EksiBtn.Location = new System.Drawing.Point(281, 147);
+            this.EksiBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.EksiBtn.Name = "EksiBtn";
+            this.EksiBtn.Size = new System.Drawing.Size(71, 75);
+            this.EksiBtn.TabIndex = 18;
+            this.EksiBtn.Text = "-";
+            this.EksiBtn.UseVisualStyleBackColor = true;
+            this.EksiBtn.Click += new System.EventHandler(this.EksiBtn_Click);
+            // 
+            // EsitBtn
+            // 
+            this.EsitBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.EsitBtn.Location = new System.Drawing.Point(160, 313);
+            this.EsitBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.EsitBtn.Name = "EsitBtn";
+            this.EsitBtn.Size = new System.Drawing.Size(71, 75);
+            this.EsitBtn.TabIndex = 12;
+            this.EsitBtn.Text = "=";
+            this.EsitBtn.UseVisualStyleBackColor = true;
+            this.EsitBtn.Click += new System.EventHandler(this.EsitBtn_Click);
+            // 
+            // NoktaBtn
+            // 
+            this.NoktaBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.NoktaBtn.Location = new System.Drawing.Point(85, 313);
+            this.NoktaBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.NoktaBtn.Name = "NoktaBtn";
+            this.NoktaBtn.Size = new System.Drawing.Size(71, 75);
+            this.NoktaBtn.TabIndex = 8;
+            this.NoktaBtn.Text = ".";
+            this.NoktaBtn.UseVisualStyleBackColor = true;
+            this.NoktaBtn.Click += new System.EventHandler(this.NoktaBtn_Click);
+            // 
+            // SifirBtn
+            // 
+            this.SifirBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SifirBtn.Location = new System.Drawing.Point(10, 313);
+            this.SifirBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.SifirBtn.Name = "SifirBtn";
+            this.SifirBtn.Size = new System.Drawing.Size(71, 75);
+            this.SifirBtn.TabIndex = 4;
+            this.SifirBtn.Text = "0";
+            this.SifirBtn.UseVisualStyleBackColor = true;
+            this.SifirBtn.Click += new System.EventHandler(this.SifirBtn_Click);
+            // 
+            // UcBtn
+            // 
+            this.UcBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.UcBtn.Location = new System.Drawing.Point(160, 230);
+            this.UcBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.UcBtn.Name = "UcBtn";
+            this.UcBtn.Size = new System.Drawing.Size(71, 75);
+            this.UcBtn.TabIndex = 11;
+            this.UcBtn.Text = "3";
+            this.UcBtn.UseVisualStyleBackColor = true;
+            this.UcBtn.Click += new System.EventHandler(this.UcBtn_Click);
+            // 
+            // İki
+            // 
+            this.İki.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.İki.Location = new System.Drawing.Point(85, 230);
+            this.İki.Margin = new System.Windows.Forms.Padding(4);
+            this.İki.Name = "İki";
+            this.İki.Size = new System.Drawing.Size(71, 75);
+            this.İki.TabIndex = 7;
+            this.İki.Text = "2";
+            this.İki.UseVisualStyleBackColor = true;
+            this.İki.Click += new System.EventHandler(this.İki_Click);
+            // 
+            // DortBtn
+            // 
+            this.DortBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.DortBtn.Location = new System.Drawing.Point(10, 147);
+            this.DortBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.DortBtn.Name = "DortBtn";
+            this.DortBtn.Size = new System.Drawing.Size(71, 75);
+            this.DortBtn.TabIndex = 2;
+            this.DortBtn.Text = "4";
+            this.DortBtn.UseVisualStyleBackColor = true;
+            this.DortBtn.Click += new System.EventHandler(this.DortBtn_Click);
+            // 
+            // BesBtn
+            // 
+            this.BesBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BesBtn.Location = new System.Drawing.Point(85, 147);
+            this.BesBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.BesBtn.Name = "BesBtn";
+            this.BesBtn.Size = new System.Drawing.Size(71, 75);
+            this.BesBtn.TabIndex = 6;
+            this.BesBtn.Text = "5";
+            this.BesBtn.UseVisualStyleBackColor = true;
+            this.BesBtn.Click += new System.EventHandler(this.BesBtn_Click);
+            // 
+            // AltiBtn
+            // 
+            this.AltiBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.AltiBtn.Location = new System.Drawing.Point(160, 147);
+            this.AltiBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.AltiBtn.Name = "AltiBtn";
+            this.AltiBtn.Size = new System.Drawing.Size(71, 75);
+            this.AltiBtn.TabIndex = 10;
+            this.AltiBtn.Text = "6";
+            this.AltiBtn.UseVisualStyleBackColor = true;
+            this.AltiBtn.Click += new System.EventHandler(this.AltiBtn_Click);
+            // 
+            // DokuzBtn
+            // 
+            this.DokuzBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.DokuzBtn.Location = new System.Drawing.Point(160, 64);
+            this.DokuzBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.DokuzBtn.Name = "DokuzBtn";
+            this.DokuzBtn.Size = new System.Drawing.Size(71, 75);
+            this.DokuzBtn.TabIndex = 9;
+            this.DokuzBtn.Text = "9";
+            this.DokuzBtn.UseVisualStyleBackColor = true;
+            this.DokuzBtn.Click += new System.EventHandler(this.DokuzBtn_Click);
+            // 
+            // SekizBtn
+            // 
+            this.SekizBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SekizBtn.Location = new System.Drawing.Point(85, 64);
+            this.SekizBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.SekizBtn.Name = "SekizBtn";
+            this.SekizBtn.Size = new System.Drawing.Size(71, 75);
+            this.SekizBtn.TabIndex = 5;
+            this.SekizBtn.Text = "8";
+            this.SekizBtn.UseVisualStyleBackColor = true;
+            this.SekizBtn.Click += new System.EventHandler(this.SekizBtn_Click);
+            // 
+            // YediBtn
+            // 
+            this.YediBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.YediBtn.Location = new System.Drawing.Point(10, 64);
+            this.YediBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.YediBtn.Name = "YediBtn";
+            this.YediBtn.Size = new System.Drawing.Size(71, 75);
+            this.YediBtn.TabIndex = 1;
+            this.YediBtn.Text = "7";
+            this.YediBtn.UseVisualStyleBackColor = true;
+            this.YediBtn.Click += new System.EventHandler(this.YediBtn_Click);
+            // 
+            // BirBtn
+            // 
+            this.BirBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BirBtn.Location = new System.Drawing.Point(10, 230);
+            this.BirBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.BirBtn.Name = "BirBtn";
+            this.BirBtn.Size = new System.Drawing.Size(71, 75);
+            this.BirBtn.TabIndex = 21;
+            this.BirBtn.Text = "1";
+            this.BirBtn.UseVisualStyleBackColor = true;
+            this.BirBtn.Click += new System.EventHandler(this.BirBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 407);
+            this.Controls.Add(this.BirBtn);
+            this.Controls.Add(this.EksiArtiBtn);
+            this.Controls.Add(this.BolBtn);
+            this.Controls.Add(this.EksiBtn);
+            this.Controls.Add(this.SilBtn);
+            this.Controls.Add(this.YuzdeBtn);
+            this.Controls.Add(this.CarpBtn);
+            this.Controls.Add(this.ArtiBtn);
+            this.Controls.Add(this.EsitBtn);
+            this.Controls.Add(this.UcBtn);
+            this.Controls.Add(this.AltiBtn);
+            this.Controls.Add(this.DokuzBtn);
+            this.Controls.Add(this.NoktaBtn);
+            this.Controls.Add(this.İki);
+            this.Controls.Add(this.BesBtn);
+            this.Controls.Add(this.SekizBtn);
+            this.Controls.Add(this.SifirBtn);
+            this.Controls.Add(this.DortBtn);
+            this.Controls.Add(this.YediBtn);
+            this.Controls.Add(this.Tb);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hesap Makinesi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Tb;
+        private System.Windows.Forms.Button SilBtn;
+        private System.Windows.Forms.Button YuzdeBtn;
+        private System.Windows.Forms.Button EksiArtiBtn;
+        private System.Windows.Forms.Button BolBtn;
+        private System.Windows.Forms.Button CarpBtn;
+        private System.Windows.Forms.Button ArtiBtn;
+        private System.Windows.Forms.Button EksiBtn;
+        private System.Windows.Forms.Button EsitBtn;
+        private System.Windows.Forms.Button NoktaBtn;
+        private System.Windows.Forms.Button SifirBtn;
+        private System.Windows.Forms.Button UcBtn;
+        private System.Windows.Forms.Button İki;
+        private System.Windows.Forms.Button DortBtn;
+        private System.Windows.Forms.Button BesBtn;
+        private System.Windows.Forms.Button AltiBtn;
+        private System.Windows.Forms.Button DokuzBtn;
+        private System.Windows.Forms.Button SekizBtn;
+        private System.Windows.Forms.Button YediBtn;
+        private System.Windows.Forms.Button BirBtn;
+    }
+}
+
---
+++ Hesap_Makinesi/Hesap Makinesi/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+        
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private decimal ilkDeger = 0.0m;
+        private decimal ikinciDeger = 0.0m;
+        private decimal sonuc = 0.0m;
+        private string operatorler = "+";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void SifirBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "0";
+            }
+            else
+            {
+                Tb.Text += "0"; 
+            }
+        }
+
+        private void NoktaBtn_Click(object sender, EventArgs e)
+        {
+            if (!Tb.Text.Contains("."))
+            {
+                Tb.Text += ".";
+            }
+        }
+
+        private void BirBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "1";
+            }
+            else
+            {
+                Tb.Text += "1";
+            }
+        }
+
+        private void İki_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "2";
+            }
+            else
+            {
+                Tb.Text += "2";
+            }
+        }
+
+        private void UcBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "3";
+            }
+            else
+            {
+                Tb.Text += "3";
+            }
+        }
+
+        private void DortBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "4";
+            }
+            else
+            {
+                Tb.Text += "4";
+            }
+        }
+
+        private void BesBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "5";
+            }
+            else
+            {
+                Tb.Text += "5";
+            }
+        }
+
+        private void AltiBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "6";
+            }
+            else
+            {
+                Tb.Text += "6";
+            }
+        }
+
+        private void YediBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "7";
+            }
+            else
+            {
+                Tb.Text += "7";
+            }
+        }
+
+        private void SekizBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "8";
+            }
+            else
+            {
+                Tb.Text += "8";
+            }
+        }
+
+        private void DokuzBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text == "0")
+            {
+                Tb.Text = "9";
+            }
+            else
+            {
+                Tb.Text += "9";
+            }
+        }
+
+        private void EksiArtiBtn_Click(object sender, EventArgs e)
+        {
+            if (Tb.Text.Contains("-"))
+            {
+                Tb.Text= Tb.Text.Trim('-');
+            }
+            else
+            {
+                Tb.Text = "-" + Tb.Text;
+            }
+        }
+
+        private void EksiBtn_Click(object sender, EventArgs e)
+        {
+            ilkDeger = decimal.Parse(Tb.Text);
+            Tb.Clear();
+            operatorler = "-";
+        }
+
+        private void ArtiBtn_Click(object sender, EventArgs e)
+        {
+            ilkDeger = decimal.Parse(Tb.Text);
+            Tb.Clear();
+            operatorler = "+";
+        }
+
+        private void BolBtn_Click(object sender, EventArgs e)
+        {
+            ilkDeger = decimal.Parse(Tb.Text);
+            Tb.Clear();
+            operatorler = "/";
+        }
+
+        private void CarpBtn_Click(object sender, EventArgs e)
+        {
+            ilkDeger = decimal.Parse(Tb.Text);
+            Tb.Clear();
+            operatorler = "*";
+        }
+
+        private void YuzdeBtn_Click(object sender, EventArgs e)
+        {
+            ilkDeger = decimal.Parse(Tb.Text);
+            Tb.Clear();
+            operatorler = "%";
+        }
+
+        private void EsitBtn_Click(object sender, EventArgs e)
+        {
+            switch (operatorler)
+            {
+                case "-":
+                    ikinciDeger = decimal.Parse(Tb.Text);
+                    sonuc = ilkDeger - ikinciDeger;
+                    Tb.Text = sonuc.ToString();
+                    break;
+                case "+":
+                    ikinciDeger = decimal.Parse(Tb.Text);
+                    sonuc = ilkDeger + ikinciDeger;
+                    Tb.Text = sonuc.ToString();
+                    break;
+                case "/":
+                    ikinciDeger = decimal.Parse(Tb.Text);
+                    sonuc = ilkDeger / ikinciDeger;
+                    Tb.Text = sonuc.ToString();
+                    break;
+                case "*":
+                    ikinciDeger = decimal.Parse(Tb.Text);
+                    sonuc = ilkDeger * ikinciDeger;
+                    Tb.Text = sonuc.ToString();
+                    break;
+                case "%":
+                    ikinciDeger = decimal.Parse(Tb.Text);
+                    sonuc = ilkDeger % ikinciDeger;
+                    Tb.Text = sonuc.ToString();
+                    break;
+            }
+        }
+
+        private void SilBtn_Click(object sender, EventArgs e)
+        {
+            ilkDeger = 0.0m;
+            ikinciDeger = 0.0m;
+            Tb.Text = "0";
+        }
+    }
+}
